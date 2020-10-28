dba3a6b25439ea8d944e69a2a1260b7ebc898e34
Brave-Y <1175151069@qq.com>
2020-10-28 18:41:32 +0800

    登陆页面功能完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,3 +5,50 @@ $('.login a').click(function () {
 $('.register a').click(function () {
     $('.login').show().next().hide();
 })
+
+//----------注册----------
+$('.register form').on('submit', function (e) {
+    e.preventDefault();
+    //收集表单数据
+    let data = $(this).serialize()
+    //ajax提交
+    $.ajax({
+        type: 'POST',
+        url: 'http://ajax.frontend.itheima.net/api/reguser',
+        data: data,
+        success: function (res) {
+            layer.msg(res.message)
+            if (res.status === 0) {
+                $('.login').show().next().hide();
+                $('.register form')[0].reset()
+            }
+        }
+    })
+})
+//---------------验证规则---------
+let form = layui.form;
+form.verify({
+    changdu: [/^\S{6,12}$/, '长度6~12位，不能有空格'],
+    same: function (val) {
+        let pwd = $('.register .pwd').val();
+        if (pwd !== val) {
+            return '两次密码不一致哦！'
+        }
+    }
+})
+//---------------------登录---------------------
+$('.login form').on('submit', function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: 'http://ajax.frontend.itheima.net/api/login',
+        data: $(this).serialize(),
+        success: function (res) {
+            layer.msg(res.message)
+            if (res.status === 0) {
+                localStorage.setItem('token', res.token);
+                location.href = '/index.html';
+            }
+        }
+    })
+})
